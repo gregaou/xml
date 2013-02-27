d3a1d18c5b32e9af87d09b9716285603b5ee7756
Benjamin Guiller <benjamin.guiben.guiller@gmail.com>
2013-02-27 18:10:26 +0100

    Java

--- ue.java
+++ ue.java
@@ -1,19 +1,12 @@
 import org.w3c.dom.*;
-import java.io.*;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 public class ue {
   public static void main(String [] args) throws Exception {
     Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(args[0]);
-    Element ue = (Element)doc.getDocumentElement().getElementsByTagName("unite").item(0);
-    System.out.println(ue);
-    Node nom = ue.getElementsByTagName("nom").item(0);
-    System.out.println(ue.getAttribute("id"));
-    System.out.println(nom.getNodeValue());
+    NodeList nodes = doc.getDocumentElement().getElementsByTagName("unite");
+    for(int i = 0; i < nodes.getLength(); ++i){
+      System.out.println(((Element)nodes.item(i)).getElementsByTagName("nom").item(0).getChildNodes().item(0).getNodeValue());
+    }
   }
 }
